f8bae93b62001499d7bf30376b2d7f204ff471a3
ridwannulloh <nulloh.ridwan0320@gmail.com>
2020-12-31 18:54:19 +0700

    Finishing simple project, getting list of news website title in Indonesia

---
+++ Scraping News Website Title/main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = [
+    'https://www.detik.com/',
+    'https://www.kompas.com/',
+    'https://www.viva.co.id/',
+    'https://www.kontan.co.id/',
+    'https://www.disway.id/',
+    'https://republika.co.id/',
+    'https://www.cnnindonesia.com/'
+]
+
+url_title = {}
+
+for link in url:
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, features='html.parser')
+    url_title[link] = soup.title.string
+
+for key, value in url_title.items():
+    print(f'Dengan memanggil = {key}, maka diperoleh judul website = {value}')
