226ce543fbd1d6109f7ac130d2030ccbb55d14ae
ridwannulloh <nulloh.ridwan0320@gmail.com>
2021-01-01 06:33:48 +0700

    Making function of Prayer Schedule Today named PSToday with type of pray as argument

--- Scraping PKPU Prayer Schedule/main.py
+++ Scraping PKPU Prayer Schedule/main.py
@@ -1,26 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://jadwalsholat.pkpu.or.id/?id=308'
-contents = requests.get(url)
+def PSToday(type):
+    url = 'https://jadwalsholat.pkpu.or.id/?id=308'
+    contents = requests.get(url)
 
-response = BeautifulSoup(contents.text, 'html.parser')
-data = response.find_all('tr', 'table_highlight')
-data = (data[0])
+    response = BeautifulSoup(contents.text, 'html.parser')
+    data = response.find_all('tr', 'table_highlight')
+    data = (data[0])
 
-sholat ={}
-i = 0
-for d in data:
-    if i == 1:
-        sholat['Subuh'] = d.get_text()
-    if i == 2:
-        sholat['Dzuhur'] = d.get_text()
-    if i == 3:
-        sholat['Ashar'] = d.get_text()
-    if i == 4:
-        sholat['Magrib'] = d.get_text()
-    if i == 5:
-        sholat['Isya'] = d.get_text()
-    i += 1
+    sholat ={}
+    i = 0
+    for d in data:
+        if i == 1:
+            sholat['Subuh'] = d.get_text()
+        if i == 2:
+            sholat['Dzuhur'] = d.get_text()
+        if i == 3:
+            sholat['Ashar'] = d.get_text()
+        if i == 4:
+            sholat['Magrib'] = d.get_text()
+        if i == 5:
+            sholat['Isya'] = d.get_text()
+        i += 1
 
-print(sholat)
+    print(f'The today schedule of {type} is at {sholat[type]}')
+
+PSToday('Isya')
