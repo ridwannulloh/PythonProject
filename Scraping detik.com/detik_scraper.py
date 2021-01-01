e6e14c3f35cd7b16a78d5b8647426d41809688ca
ridwannulloh <nulloh.ridwan0320@gmail.com>
2021-01-01 07:45:28 +0700

    Adding flask framework to deploy scraper to local machine

---
+++ Scraping detik.com/detik_scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+html_doc = requests.get('https://www.detik.com/terpopuler?', params={'tag_from': 'wp_cb_mostPopular_more'})
+
+soup = BeautifulSoup(html_doc.text, 'html.parser')
+popular_area = soup.find(attrs={'class': "grid-row list-content"})
+
+titles = popular_area.find_all(attrs={'class': 'media__title'})
+images = popular_area.find_all(attrs={'class': 'media__image'})
+
+for image in images:
+    print(image.find('a').find('img')['title'])
+    print(image.find('a')['href'])
+    print(image.find('a').find('img')['src'])
+    print('-' * 20)
